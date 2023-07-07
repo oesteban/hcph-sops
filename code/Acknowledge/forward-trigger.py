5585dc175e9d77f651b216143f41158666151459
esavary <elodie.savary@outlook.com>
2023-07-07 18:51:57 +0200

    ENH: new gestion of triggers with asyncio

--- code/Acknowledge/forward-trigger.py
+++ code/Acknowledge/forward-trigger.py
@@ -1,34 +1,55 @@
-import sys
-import serial
-import keyboard
 import asyncio
+import serial
+import threading
+import keyboard
 
-port = "/dev/ttyACM0"
+async def handle_client(reader, signal_queue):
+    while True:
+        data = await reader.read(1)
 
-def send_trigger():
-    with serial.Serial(port) as ser:
-        trigger = b'\x01'
-        ser.write(trigger)
+        if not data:
+            await asyncio.sleep(0.1)  # Add a small delay to avoid busy waiting
+            continue
 
-async def handle_key_press():
-    keyboard.add_hotkey("s", send_trigger)
-    await asyncio.Event().wait()
+        signal = data  # No need for conversion if the signal is already in binary
+        #print(f"Received signal: {signal}")
+        await signal_queue.put(signal)
+
+async def start_server(host, port, signal_queue):
+    server = await asyncio.start_server(lambda r, w: handle_client(r, signal_queue), host, port)
+    addr = server.sockets[0].getsockname()
+    print(f"Server listening on {addr}")
+    async with server:
+        await server.serve_forever()
+
+async def forward_signals(serial_port, signal_queue):
+    with serial.Serial(serial_port) as ser:
+        while True:
+            signal = await signal_queue.get()
+            ser.write(signal)
+
+async def keyboard_input(signal_queue):
+    while True:
+        if keyboard.is_pressed("s"):
+            await asyncio.sleep(0.1)
+            #print(f"s pressed")
+            await signal_queue.put(b"\x01")
+        else:
+            await asyncio.sleep(0.1)
+              # Enqueue b"\x01" into the signal_queue
 
 async def main():
+    serial_port = '/dev/ttyACM0'
+    signal_queue = asyncio.Queue()
+
     await asyncio.gather(
-        handle_key_press(),
-        asyncio.sleep(0.008)
+        start_server("localhost", 8888, signal_queue),
+        forward_signals(serial_port, signal_queue),
+        keyboard_input(signal_queue)
     )
 
-if __name__ == '__main__':
-    print("Main loop")
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(main())
-    except KeyboardInterrupt:
-        loop.stop()
-    finally:
-        loop.close()
+if __name__ == "__main__":
+    asyncio.run(main())
 
 
 
