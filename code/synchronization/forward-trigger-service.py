164d8f1d7e09528118efa5547dc1c11b0c79ef8a
Oscar Esteban <code@oscaresteban.es>
2023-07-14 09:52:30 +0200

    maint: fix file path and run black for style

--- code/synchronization/forward-trigger-service.py
+++ code/synchronization/forward-trigger-service.py
@@ -3,6 +3,7 @@ import serial
 import threading
 import keyboard
 
+
 async def handle_client(reader, signal_queue):
     while True:
         data = await reader.read(1)
@@ -12,15 +13,19 @@ async def handle_client(reader, signal_queue):
             continue
 
         signal = data  # No need for conversion if the signal is already in binary
-        #print(f"Received signal: {signal}")
+        # print(f"Received signal: {signal}")
         await signal_queue.put(signal)
 
+
 async def start_server(host, port, signal_queue):
-    server = await asyncio.start_server(lambda r, w: handle_client(r, signal_queue), host, port)
+    server = await asyncio.start_server(
+        lambda r, w: handle_client(r, signal_queue), host, port
+    )
     addr = server.sockets[0].getsockname()
     print(f"Server listening on {addr}")
     async with server:
         await server.serve_forever()
+
 
 async def forward_signals(serial_port, signal_queue):
     with serial.Serial(serial_port) as ser:
@@ -28,29 +33,28 @@ async def forward_signals(serial_port, signal_queue):
             signal = await signal_queue.get()
             ser.write(signal)
 
+
 async def keyboard_input(signal_queue):
     while True:
         if keyboard.is_pressed("s"):
             await asyncio.sleep(0.1)
-            #print(f"s pressed")
+            # print(f"s pressed")
             await signal_queue.put(b"\x01")
         else:
             await asyncio.sleep(0.1)
-              # Enqueue b"\x01" into the signal_queue
+            # Enqueue b"\x01" into the signal_queue
+
 
 async def main():
-    serial_port = '/dev/ttyACM0'
+    serial_port = "/dev/ttyACM0"
     signal_queue = asyncio.Queue()
 
     await asyncio.gather(
         start_server("localhost", 8888, signal_queue),
         forward_signals(serial_port, signal_queue),
-        keyboard_input(signal_queue)
+        keyboard_input(signal_queue),
     )
+
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
-
-
