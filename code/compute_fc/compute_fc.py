85ae6962ab918f6d62dc61c4274b68c3ce3c6281
Alexandre Cionca <alexandre.cionca@gmail.com>
2023-09-08 10:12:45 +0200

    enh: improved verbose

--- code/compute_fc/compute_fc.py
+++ code/compute_fc/compute_fc.py
@@ -4,7 +4,11 @@ import logging
 import os
 import os.path as op
 
+import matplotlib.pyplot as plt
+
 from bids import BIDSLayout
+from bids.layout import parse_file_entities
+from bids.layout.writing import build_path
 
 from nilearn.datasets import fetch_atlas_difumo
 from nilearn.maskers import NiftiMapsMasker
@@ -12,6 +16,8 @@ from nilearn.interfaces.fmriprep import load_confounds
 
 from nilearn.connectome import ConnectivityMeasure
 from sklearn.covariance import GraphicalLassoCV
+
+#from nilearn.plotting import plot_design_matrix, plot_matrix
 
 def get_arguments():
     parser = argparse.ArgumentParser(
@@ -21,85 +27,52 @@ def get_arguments():
     )
 
     parser.add_argument('data_dir')
+    parser.add_argument('-s', '--save', action='store_true', default=False)
+    parser.add_argument('-o', '--output', default=None)
     parser.add_argument('-v', '--verbosity', action="count", default=1,
                         help="""increase output verbosity (-v: standard logging infos; 
                         -vv: logging infos and NiLearn verbose; -vvv: debug)""")
-    parser.add_argument('--no-bids', action='store_true', default=False)
+    parser.add_argument('--task', default=[], action='store', nargs='+')
     parser.add_argument('--atlas-dimension', default=64)
 
     args = parser.parse_args()
 
     return args
 
-def get_folders_w_prefix(path, prefix, stop_at_first=True):
-    subfolders = os.walk(path)
-
-    matching_dirs = []
-    for root, dirs, files in subfolders:
-        #logging.debug(f'Looking for "{prefix}" in {root}')
-        has_sub = [subdir for subdir in dirs  if subdir.startswith(prefix)]
-        if len(has_sub) > 0:
-            logging.debug(f'Found {len(has_sub)} "{prefix}" folder(s) in {root}')
-            matching_dirs += [op.join(root, target_dir) for target_dir in has_sub]
-            if stop_at_first:
-                return matching_dirs
-
-    if len(matching_dirs) == 0:
-        logging.warning(f'No folder found containing "{prefix}"')
-    return matching_dirs
-
-def find_func_files(paths_to_func_dir, suffix):
-    paths_to_func_dir = list(paths_to_func_dir)
-
-    matching_files = []
-    for path in paths_to_func_dir:
-        files_in_func = os.listdir(path)
-
-        # Find filename that matches prefix
-        matching_files += [op.join(path, filename) for filename in files_in_func if suffix in filename]
-    
-    # Filter matching files keeping only neuroimaging formats (e.g. .nii.gz)
-    matching_ni = [filename for filename in matching_files if '.nii' in filename]
-
-    logging.debug(f'Found {len(matching_ni)} matching functional file(s):')
-    logging.debug('\t'+'\n\t'.join(matching_ni))
-    return matching_ni
-
-def get_func_filenames(input_path):
-    logging.info(f'Manually scan the folders to find functional files...')
-    logging.debug(f'Scanning for data in {input_path} subfolders')
-    path_to_subs = get_folders_w_prefix(input_path, prefix='sub-')
-    path_to_sessions = []
-
-    for path_to_individual_sub in path_to_subs:
-        path_to_sessions += get_folders_w_prefix(path_to_individual_sub, prefix='ses-')
-
-    logging.debug(f'Pipeline will continue with {len(path_to_sessions)} sessions at:')
-    logging.debug('\t'+'\n\t'.join(path_to_sessions))
-
-    path_to_func = [op.join(ses, 'func') for ses in path_to_sessions]
-
-    func_filenames = find_func_files(path_to_func, suffix='MNI152NLin2009cAsym_desc-preproc_bold')
-
-    return func_filenames
-
-def get_func_filenames_bids(paths_to_func_dir):
-    logging.info(f'Using BIDS to find functional files...')
+def get_func_filenames_bids(paths_to_func_dir, task_filter=[]):
+    logging.debug(f'Using BIDS to find functional files...')
 
     layout = BIDSLayout(
         paths_to_func_dir,
         validate=False,
         )
-
+    
     all_derivatives = layout.get(
         scope='all',
         return_type='file',
         extension='nii.gz',
         session=[15],
-        suffix='bold'
+        suffix='bold',
+        task=task_filter
         )
 
     return all_derivatives
+
+def get_bids_savename(filename,
+                      patterns=None, **kwargs):
+    if patterns is None:
+        patterns = ['sub-{subject}[/ses-{session}]/func/sub-{subject}'
+                    '[_ses-{session}][_task-{task}][_meas-{meas}]'
+                    '_{suffix}{extension}']
+    entity = parse_file_entities(filename)
+
+    for key, value in kwargs.items():
+        entity[key] = value
+
+    bids_savename = build_path(entity, patterns)
+    logging.debug(f'save filename will be:\n\t{build_path(entity, patterns)}')
+
+    return bids_savename
 
 def get_atlas_data(atlas_name='DiFuMo', **kwargs):
     logging.info(f'Fetching the DiFuMo atlas ...')
@@ -108,7 +81,15 @@ def get_atlas_data(atlas_name='DiFuMo', **kwargs):
         logging.warning(f'Dimension for DiFuMo atlas is different from 64, 128 or 512 !')
 
     return fetch_atlas_difumo(legacy_format=False, **kwargs)
-    
+
+def find_derivative(path, derivatives_name='derivatives'):
+    if op.split(path)[-1] == derivatives_name:
+        return path
+    if op.split(path)[0]=='':
+        logging.error(f'"{derivatives_name}" could not be found on path !')
+        return ''
+    return find_derivative(path=op.split(path)[0])
+
 def extract_timeseries(func_filename, atlas_filename, masker=None, verbose=2):
     if masker is None:
         masker = NiftiMapsMasker(
@@ -119,12 +100,15 @@ def extract_timeseries(func_filename, atlas_filename, masker=None, verbose=2):
             )
     
     if type(func_filename) == list:
-        time_serie = []
+        time_series = []
+        confounds = []
         for file_id, individual_filename in enumerate(func_filename):
             logging.info(f'Extraction of timeseries from list: {file_id+1}/{len(func_filename)} ...')
-            time_serie.append(extract_timeseries(individual_filename, atlas_filename,
-                                                 masker=masker, verbose=verbose))
-        return time_serie
+            ts, conf = extract_timeseries(individual_filename, atlas_filename,
+                                          masker=masker, verbose=verbose)
+            time_series.append(ts)
+            confounds.append(conf)
+        return time_series, confounds
 
     confounds, sample_mask = load_confounds(
         func_filename,
@@ -135,13 +119,13 @@ def extract_timeseries(func_filename, atlas_filename, masker=None, verbose=2):
         std_dvars_threshold=10,
         )
     
-    time_serie = masker.fit_transform(
+    time_series = masker.fit_transform(
         func_filename,
         confounds=confounds,
         sample_mask=sample_mask
         )
     
-    return time_serie
+    return time_series, confounds
 
 def compute_connectivity(time_series, strategy='sparse inverse covariance'):
 
@@ -161,11 +145,28 @@ def compute_connectivity(time_series, strategy='sparse inverse covariance'):
 
     return estimator.precision_
 
+def visual_report_timeserie(timeseries, confounds=None, save=False, output=None):
+
+    plt.imshow(timeseries, cmap='binary')
+
+    if save is not None:
+        plt.savefig(output)
+
+    return
+
+def visual_report_fc(matrix, save=False, output=None):
+    return
+
+def save_output(data_list, original_filenames, output=None):
+    return
+
 def main():
     args = get_arguments()
 
     input_path = args.data_dir
-    no_bids = args.no_bids
+    save = args.save
+    output = args.output
+    task_filter = args.task
     atlas_dimension = args.atlas_dimension
     verbosity_level = args.verbosity
     nilearn_verbose = verbosity_level-1
@@ -182,29 +183,37 @@ def main():
         level=logging_level_map[min([verbosity_level, 3])]
     )
 
-    if no_bids:
-        func_filenames = get_func_filenames(input_path)
-    else:
-        func_filenames = get_func_filenames_bids(input_path)
+    func_filenames = get_func_filenames_bids(input_path, task_filter=task_filter)
 
     logging.info(f'Found {len(func_filenames)} functional file(s):')
     logging.info('\t'+'\n\t'.join([op.basename(filename) for filename in func_filenames]))
 
     atlas_data = get_atlas_data(dimension=atlas_dimension)
     atlas_filename = getattr(atlas_data, 'maps')
+
+    if save:
+        if output is None:
+            output = op.join(find_derivative(input_path), 'functional_connectivity')
+        logging.info(f'Output will be save at:\n\t{output}')
+        #os.makedirs(output, exist_ok=True)
         
-    #all_time_series = extract_timeseries(func_filenames, atlas_filename,
-    #                                     verbose=nilearn_verbose)
-    
-    all_time_series = []
-    for individual_time_serie in all_time_series:
-        print(individual_time_serie.shape)
+    #all_time_series, all_confounds = extract_timeseries(func_filenames,
+    #                                                    atlas_filename,
+    #                                                    verbose=nilearn_verbose)
+    #
+    #for individual_time_serie, confounds in zip(all_time_series, all_confounds):
+    #    visual_report_timeserie(individual_time_serie, confounds=confounds,
+    #                            save=save, output=output)
+    #    print(individual_time_serie.shape)
 
     #all_fc_matrices = compute_connectivity(all_time_series)
     
     all_fc_matrices = []
-    for individual_matrix in all_fc_matrices:
-        print(individual_matrix.shape)
+    for individual_matrix, individual_file in zip(all_fc_matrices, func_filenames):
+        print(f'matrix has shape {individual_matrix.shape} and will be saved at:')
+        print(get_bids_savename(filename=individual_file, suffix='relmat',
+                                meas='sparseinversecovariance',
+                                extension='.tsv'))
     
 if __name__ == "__main__":
     main()
