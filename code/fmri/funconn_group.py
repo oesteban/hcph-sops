1a2c5b1ee449bde0e087ea347317b5be03d49c81
celprov <celine.provins@unil.ch>
2024-04-15 17:55:19 +0200

    enh: assemble the figures for QC of FC into a HTML report using nireports

--- code/fmri/funconn_group.py
+++ code/fmri/funconn_group.py
@@ -19,10 +19,7 @@ from load_save import (
 )
 
 from reports import (
-    group_report_censoring,
-    group_report_fc_dist,
-    group_report_qc_fc,
-    group_report_qc_fc_euclidean,
+    group_report,
 )
 
 
@@ -138,11 +135,13 @@ def main():
     iqms_df = load_iqms(output, existing_fc, mriqc_path=mriqc_path)
 
     # Generate group figures
-    group_report_censoring(good_timepoints_df, output)
-    group_report_fc_dist(fc_matrices, output)
-    qc_fc_dict = group_report_qc_fc(fc_matrices, iqms_df, output)
-    group_report_qc_fc_euclidean(qc_fc_dict, atlas_filename, output)
-
+    group_report(
+        good_timepoints_df,
+        fc_matrices,
+        iqms_df,
+        atlas_filename,
+        output,
+    )
 
 if __name__ == "__main__":
     main()
