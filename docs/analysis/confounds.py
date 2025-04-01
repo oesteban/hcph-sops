515ea8f0cc4c1e4b14db89f7a6b8180c75bd6b75
Céline Provins <celine.provins@unil.ch>
2025-04-01 14:07:11 +0200

    sty: modularize the code by separating independent chunk of codes in separated functions

--- docs/analysis/confounds.py
+++ docs/analysis/confounds.py
@@ -1,10 +1,37 @@
-def get_confounds(dataset_path="/data/datasets/hcph-dataset", iqms_path=None, iqm_of_interest=["fd_mean"]):
-    import pandas as pd
-    from pathlib import Path
+import pandas as pd
+from pathlib import Path
 
+def get_confounds_scanstsv(dataset_path="/data/datasets/hcph-dataset"):
+    """
+    Extract and process confound information from a `scans.tsv` file.
+
+    This function reads a `scans.tsv` file from the specified dataset path, processes
+    its contents to extract relevant confound information, and returns a DataFrame
+    with additional metadata columns.
+
+    Parameters:
+    -----------
+    dataset_path : str, optional
+        The path to the dataset directory containing the `scans.tsv` file.
+        Default is "/data/datasets/hcph-dataset".
+
+    Returns:
+    --------
+    pd.DataFrame
+        A pandas DataFrame containing the processed confound information with the following columns:
+        - `filename`: The original filename of the scan.
+        - `acq_time`: The acquisition time of the scan.
+        - `subject`: The subject identifier extracted from the filename.
+        - `session`: The session identifier extracted from the filename.
+        - `modality`: The modality of the scan (e.g., anat, func) extracted from the filename.
+        - `task`: The task identifier extracted from the filename.
+        - `pe_dir`: The phase encoding direction extracted from the filename.
+        - `day_of_week`: The day of the week extracted from the acquisition time.
+        - `time_of_day`: The rounded time of day extracted from the acquisition time.
+    """
+
+    ## Read the scans.tsv file
     dataset_path = Path(dataset_path)
-
-    # Read the scans.tsv file
     confounds_df = pd.read_csv(dataset_path / "scans.tsv", sep="\t")
     confounds_df.drop(columns=["randstr"], inplace=True)
 
@@ -30,17 +57,68 @@ def get_confounds(dataset_path="/data/datasets/hcph-dataset", iqms_path=None, iq
     )
     confounds_df.drop(columns=["datetime"], inplace=True)
 
-    # If iqms_path is provided, read the IQMs and merge them with the confounds
+    return confounds_df
+
+
+def get_iqms(iqms_path="/data/derivatives/hcph-mriqc/group_dwi.tsv", iqm_of_interest=["fd_mean"]):
+    """
+    Extract imaging quality metrics (IQMs) from a specified TSV file.
+    This function reads a TSV file containing IQMs, extracts relevant metadata 
+    (subject, session, modality, and task) from the `bids_name` column, and filters 
+    the data to include only the specified IQMs of interest.
+    Parameters:
+    -----------
+    iqms_path : str, optional
+        Path to the TSV file containing the IQMs. Default is 
+        "/data/derivatives/hcph-mriqc/group_dwi.tsv".
+    iqm_of_interest : list of str, optional
+        List of column names corresponding to the IQMs to retain in the output. 
+        Default is ["fd_mean"].
+    Returns:
+    --------
+    pd.DataFrame
+        A pandas DataFrame containing the extracted metadata (subject, session, 
+        modality, task) and the specified IQMs of interest.
+    """
+
+    iqms_df = pd.read_csv(iqms_path, sep="\t")
+    iqms_df = iqms_df.assign(
+        subject=iqms_df["bids_name"].str.extract(r"sub-(\d+)_"),
+        session=iqms_df["bids_name"].str.extract(r"ses-(\w+)_"),
+        modality=iqms_df["bids_name"].str.split("_").str[-1],
+        task=iqms_df["bids_name"].str.extract(r"task-(\w+)_"),
+    )
+    # Keep only the IQMs of interest
+    iqms_df = iqms_df[["subject", "session", "modality", "task"] + iqm_of_interest]
+
+    return iqms_df
+
+
+def get_confounds(dataset_path="/data/datasets/hcph-dataset", iqms_path=None, iqm_of_interest=["fd_mean"]):
+    """
+    Extract and merge confounds from various sources for a given dataset.
+    This function retrieves confounds from a dataset, optionally merges them with 
+    image quality metrics (IQMs) if provided, and includes additional confounds 
+    found in GitHub issue logs.
+    Parameters:
+        dataset_path (str): 
+            Path to the dataset directory. Defaults to "/data/datasets/hcph-dataset".
+        iqms_path (str, optional): 
+            Path to the file containing IQMs. If provided, the IQMs will be merged 
+            with the confounds DataFrame. Defaults to None.
+        iqm_of_interest (list of str): 
+            List of IQMs to extract from the IQMs file. Defaults to ["fd_mean"].
+    Returns:
+        pandas.DataFrame: 
+            A DataFrame containing the merged confounds from the dataset, IQMs 
+            (if provided), and mood-related confounds.
+    """
+
+    confounds_df = get_confounds_scanstsv(dataset_path)
+
+    ## If iqms_path is provided, read the IQMs and merge them with the confounds
     if iqms_path:
-        iqms_df = pd.read_csv(iqms_path, sep="\t")
-        iqms_df = iqms_df.assign(
-            subject=iqms_df["bids_name"].str.extract(r"sub-(\d+)_"),
-            session=iqms_df["bids_name"].str.extract(r"ses-(\w+)_"),
-            modality=iqms_df["bids_name"].str.split("_").str[-1],
-            task=iqms_df["bids_name"].str.extract(r"task-(\w+)_"),
-        )
-        # Keep only the IQMs of interest
-        iqms_df = iqms_df[["subject", "session", "modality", "task"] + iqm_of_interest]
+        iqms_df = get_iqms(iqms_path, iqm_of_interest)
 
         # Merge the fd_mean values into the confounds DataFrame
         confounds_df = pd.merge(
